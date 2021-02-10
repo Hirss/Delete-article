e188808f0fa6c9412ed151ec807ea1792c3d6e46
Mev <36679013+Hirss@users.noreply.github.com>
2021-02-10 17:09:17 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -4,6 +4,6 @@ if(isset($_GET['id']) AND !empty($_GET['id'])) {
 	$suppr_id = htmlspecialchars($_GET['id']);
 	$suppr = $bdd->prepare('DELETE FROM db WHERE id = ?');
 	$suppr->execute(array($suppr_id));
-	header('Location: indexs.php.php');
+	header('Location: indexs.php');
 }
 ?>
